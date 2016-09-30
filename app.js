d08f36b41b7bbbd0964d08851b06eb33f3d8698f
Gordon Zheng <gordon@greenhouse.io>
2016-09-30 16:45:04 -0400

    Add proxy which appends header

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const url = require('url');
 const express = require('express');
 const app = express();
 
@@ -5,8 +6,23 @@ const PORT = process.env.PORT || 8080;
 const DESTINATION_URL = 'https://requestbin.herokuapp.com/1g4lmbx1';
 const ACCESS_TOKEN = 'SOMETOKEN';
 
+const httpProxy = require('http-proxy');
+const proxy = httpProxy.createProxyServer({
+  target: url.parse(DESTINATION_URL),
+  ignorePath: true,
+  changeOrigin: true
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.post('/proxy', (req, res) => {
+  proxy.web(req, res);
+})
+
+proxy.on('proxyReq', function(proxyReq, req, res, options) {
+  proxyReq.setHeader('Authorization', 'Bearer ${ACCESS_TOKEN}');
 });
 
 app.listen(PORT, () => console.log('Listening on', PORT));
