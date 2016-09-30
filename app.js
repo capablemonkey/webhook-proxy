b838f77456063befe1947a98dbc45dd96e02559b
Gordon Zheng <gordon@greenhouse.io>
2016-09-30 16:47:17 -0400

    Expose config variables

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 8080;
-const DESTINATION_URL = 'https://requestbin.herokuapp.com/1g4lmbx1';
-const ACCESS_TOKEN = 'SOMETOKEN';
+const DESTINATION_URL = process.env.DESTINATION_URL ||'https://requestbin.herokuapp.com/1g4lmbx1';
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN ||'SOMETOKEN';
 
 const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({
@@ -22,7 +22,7 @@ app.post('/proxy', (req, res) => {
 })
 
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
-  proxyReq.setHeader('Authorization', 'Bearer ${ACCESS_TOKEN}');
+  proxyReq.setHeader('Authorization', `Bearer ${ACCESS_TOKEN}`);
 });
 
 app.listen(PORT, () => console.log('Listening on', PORT));
