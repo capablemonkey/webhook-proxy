123253beb7304f97a7a5c6fee0d5c5ec6cbe3686
Gordon Zheng <gordon@greenhouse.io>
2016-09-30 16:08:09 -0400

    Add port, destination and token as constants

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
+
+const PORT = process.env.PORT || 8080;
+const DESTINATION_URL = 'https://requestbin.herokuapp.com/1g4lmbx1';
+const ACCESS_TOKEN = 'SOMETOKEN';
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(port, () => console.log('Listening on', port));
+app.listen(PORT, () => console.log('Listening on', PORT));
