1e28752100d8f395f6005601dcb429f88b1bdf2a
Gordon Zheng <gordon@greenhouse.io>
2016-10-04 16:57:10 -0400

    Add skeleton for generating access token

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 const DESTINATION_URL = process.env.DESTINATION_URL ||'https://requestbin.herokuapp.com/1g4lmbx1';
-const ACCESS_TOKEN = process.env.ACCESS_TOKEN ||'SOMETOKEN';
+
+const CLIENT_ID = process.env.CLIENT_ID || '' ;
+const CLIENT_SECRET = process.env.CLIENT_SECRET || '' ;
+const USERNAME = process.env.USERNAME || '' ;
+const PASSWORD = process.env.PASSWORD || '' ;
+const TOKEN_ENDPOINT = process.env.TOKEN_ENDPOINT || '' ;
+const REFRESH_INTERVAL_HOURS = 12;
+
+var accessToken = process.env.ACCESS_TOKEN ||'SOMETOKEN';
 
 const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({
@@ -22,7 +30,14 @@ app.post('/proxy', (req, res) => {
 })
 
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
-  proxyReq.setHeader('Authorization', `Bearer ${ACCESS_TOKEN}`);
+  proxyReq.setHeader('Authorization', `Bearer ${accessToken}`);
 });
+
+function generateAccessToken() {
+  // TODO:
+  accessToken = 'pretend';
+}
+
+setInterval(generateAccessToken, REFRESH_INTERVAL_HOURS * 60 * 60 * 1000);
 
 app.listen(PORT, () => console.log('Listening on', PORT));
